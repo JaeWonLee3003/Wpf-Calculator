81347d27bb406200675ae65b536ec55c0ad02f84
재원 이 <kenneth.lee@latisglobal.net>
2023-09-21 13:59:26 +0900

    UI 모두 구현 완료 및 사칙연산 추가

--- calc/MainWindow.xaml.cs
+++ calc/MainWindow.xaml.cs
@@ -20,25 +20,60 @@ namespace calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastresult;
+        double result;
+
         public MainWindow()
         {
             InitializeComponent();
             ButtonCreate();
-            
+            CalcTextBox.Text = "0";
+
         }
 
+        public enum SelectedOperator
+        {
+            Addition,
+            Subtraction,
+            Multiplication,
+            Division
+        }
+
+        public class SimpleMath
+        {
+            public static double Add(double n1, double n2)
+            {
+                return n1 + n2;
+            }
+            public static double Subtract(double n1, double n2)
+            {
+                return n1 - n2;
+            }
+            public static double Multiple(double n1, double n2)
+            {
+                return n1 * n2;
+            }
+            public static double Divide(double n1, double n2)
+            {
+                return n1 / n2;
+            }
+
+        }
         private void ButtonCreate()
         {
             /* Number Button Create Method */
-            NineButton.Click += NineButton_Click;
-            EightButton.Click += EightButton_Click;
-            SevenButton.Click += SevenButton_Click;
-            SixButton.Click += SixButton_Click;
-            FiveButton.Click += FiveButton_Click;
-            FourButton.Click += FourButton_Click;
-            ThreeButton.Click += ThreeButton_Click;
-            TwoButton.Click += TwoButton_Click;
-            OneButton.Click += OneButton_Click;
+            NineButton.Click += NumberButton_Click;
+            EightButton.Click += NumberButton_Click;
+            SevenButton.Click += NumberButton_Click;
+            SixButton.Click += NumberButton_Click;
+            FiveButton.Click += NumberButton_Click;
+            FourButton.Click += NumberButton_Click;
+            ThreeButton.Click += NumberButton_Click;
+            TwoButton.Click += NumberButton_Click;
+            OneButton.Click += NumberButton_Click;
+            ZeroButton.Click += NumberButton_Click;
+
+
 
             /* Four basic operations Button Create Method */
             PlusButton.Click += PlusButton_Click;
@@ -49,6 +84,121 @@ namespace calc
             PlusMinusButton.Click += PlusMinusButton_Click;
             DotButton.Click += DotButton_Click;
             ResultButton.Click += ResultButton_Click;
+            ClearButton.Click += ClearButton_Click;
+            ClearEntryButton.Click += ClearEntryButton_Click;
+
+        }
+
+        private void ClearEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            CalcTextBox.Text = "0";
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. 어떤 버튼을 눌러는지 파악
+            // 1.1 TextBox에 표시된 수를 확인해야함
+            //      "0" -> 1를 누름 -> "1"
+            //      "1" -> 2를 누름 -> "12"
+            //
+            // 2. UI에 표시됨
+            double input = 0;
+            if (CalcTextBox.Text == "0")
+            {
+                if (sender == NineButton)
+                {
+                    input = 9;
+                }
+                if (sender == EightButton)
+                {
+                    input = 8;
+                }
+                if (sender == SevenButton)
+                {
+                    input = 7;
+                }
+                if (sender == SixButton)
+                {
+                    input = 6;
+                }
+                if (sender == FiveButton)
+                {
+                    input = 5;
+                }
+                if (sender == FourButton)
+                {
+                    input = 4;
+                }
+                if (sender == ThreeButton)
+                {
+                    input = 3;
+                }
+                if (sender == TwoButton)
+                {
+                    input = 2;
+                }
+                if (sender == OneButton)
+                {
+                    input = 1;
+                }
+                if (sender == ZeroButton)
+                {
+                    input = 0;
+                }
+                CalcTextBox.Text = input.ToString();
+            }
+            else
+            {
+                if (sender == NineButton)
+                {
+                    input = 9;
+                }
+                if (sender == EightButton)
+                {
+                    input = 8;
+                }
+                if (sender == SevenButton)
+                {
+                    input = 7;
+                }
+                if (sender == SixButton)
+                {
+                    input = 6;
+                }
+                if (sender == FiveButton)
+                {
+                    input = 5;
+                }
+                if (sender == FourButton)
+                {
+                    input = 4;
+                }
+                if (sender == ThreeButton)
+                {
+                    input = 3;
+                }
+                if (sender == TwoButton)
+                {
+                    input = 2;
+                }
+                if (sender == OneButton)
+                {
+                    input = 1;
+                }
+                if (sender == ZeroButton)
+                {
+                    input = 0;
+                }
+                CalcTextBox.Text += input.ToString();
+            }
+
+             
+            
         }
 
         /* Calculator Method*/
@@ -93,6 +243,8 @@ namespace calc
             throw new NotImplementedException();
         }
 
+
+
         /* Etc Button Methods */
         private void DivisButton_Click(object sender, RoutedEventArgs e)
         {
@@ -119,6 +271,5 @@ namespace calc
             throw new NotImplementedException();
         }
         
-
     }
 }
